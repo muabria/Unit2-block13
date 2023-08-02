f191d2a9e146d3fc0e3115625ce9d41489e438a0
brianna <briarusss@gmail.com>
2023-08-02 01:59:48 -0400

    javascript.js file

---
+++ javascript.js
@@ -0,0 +1,29 @@
+//using if and else statements to create a javascript code
+
+//if value is truthy, print true
+//if value if false print the following
+//"The boolean value false is falsy" 
+//"The null value is falsy";
+//"undefined is falsy";
+//"The number 0 is falsy (the only falsy number)"
+//The empty string is falsy (the only falsy string)
+
+
+
+const value = false
+if (value) {
+    console.log("The null value is falsy")
+} else if (value1) { 
+console.log( "the boolean value is falsy" )
+}
+
+const value1 = false
+if (value1) { 
+    
+}
+
+const value2 = false
+if(value2) { 
+    console.log("The number 0 is falsy (the only falsy number")
+}
+const value3 = true
